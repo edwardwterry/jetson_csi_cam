3f444c15e741b7cbe4de41762da93bc6082e0d68
Edward Terry <edward.william.terry@gmail.com>
2020-06-26 16:19:17 -0700

    Add Python repeater node to publish time-synced CameraInfo messages

---
+++ src/publish_camera_info.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import Image, CameraInfo
+import yaml
+
+calibration_file = '/home/ed/.ros/camera_info/camera.yaml'
+with open(calibration_file, 'r') as f:
+    data = yaml.safe_load(f)
+
+def callback(msg):
+    info = CameraInfo()
+    info.header = msg.header
+    info.height = data['image_height']
+    info.width = data['image_width']
+    info.distortion_model = data['distortion_model']
+    info.D = data['distortion_coefficients']['data']
+    info.K = data['rectification_matrix']['data']
+    info.P = data['projection_matrix']['data']
+    info_pub.publish(info)
+
+if __name__ == '__main__':
+    rospy.init_node('publish_camera_info', anonymous=True)
+    image_sub = rospy.Subscriber('image_raw_throttle', Image, callback)
+    info_pub = rospy.Publisher('camera_info', CameraInfo, queue_size=1)
+    rospy.spin()
